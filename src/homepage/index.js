cf046455d187ebe60869d931a13ec58066c69b37
isaacrdz <zack_cpp@hotmail.com>
2016-12-27 23:05:57 -0600

    added title

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,6 +1,8 @@
 var page = require('page');
+var title = require('title');
 
 page('/', function(ctx, next) {
-    var main = document.getElementById('main-container');
-    main.innerHTML = '<a href="/signup">Signup</a>';
+  title('Platzigram - Home')
+  var main = document.getElementById('main-container');
+  main.innerHTML = '<a href="/signup">Signup</a>';
 })
