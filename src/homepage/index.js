2441dcb8c66ced42a5907ac18c74336efc247847
isaacrdz <zack_cpp@hotmail.com>
2016-12-29 13:08:34 -0600

    adde formatjs with intl in safari

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -20,7 +20,7 @@ page('/', function(ctx, next){
     },
     {
       user:{
-        username:"zackrdz",
+        username:"kcamacho",
         avatar:"https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-1/c0.94.308.308/1918648_311187890598_6661099_n.jpg?oh=e6c4d792b8703c041fc40bebe3b02fef&oe=5920447D",
       },
       url:"http://materializecss.com/images/office.jpg",
