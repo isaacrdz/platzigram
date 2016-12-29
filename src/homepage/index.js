cbb2a56539bee94e3669f188ab613e83eaf97a9b
isaacrdz <zack_cpp@hotmail.com>
2016-12-29 00:06:17 -0600

    added image to timeline

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,5 +6,26 @@ var title = require('title');
 page('/', function(ctx, next){
   title('Platzigram')
   var main = document.getElementById('main-container');
-  empty(main).appendChild(template);
+
+  pictures = [
+    {
+      user:{
+        username:"zackrdz",
+        avatar:"https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-1/c0.94.308.308/1918648_311187890598_6661099_n.jpg?oh=e6c4d792b8703c041fc40bebe3b02fef&oe=5920447D",
+      },
+      url:"http://materializecss.com/images/office.jpg",
+      likes:125,
+      liked:true
+    },
+    {
+      user:{
+        username:"zackrdz",
+        avatar:"https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-1/c0.94.308.308/1918648_311187890598_6661099_n.jpg?oh=e6c4d792b8703c041fc40bebe3b02fef&oe=5920447D",
+      },
+      url:"http://materializecss.com/images/office.jpg",
+      likes:1234,
+      liked:true
+    },
+  ];
+  empty(main).appendChild(template(pictures));
 })
