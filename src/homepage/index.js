b8b1367c62762434fa577fff67c59a05fed41477
isaacrdz <zack_cpp@hotmail.com>
2017-01-03 22:23:12 -0600

    async_await

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,11 +2,10 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
-var request = require('superagent');
 var header = require('../header');
 var axios = require('axios');
 
-page('/', header, loadPicturesFetch, function(ctx, next){
+page('/', header, asyncLoad, function(ctx, next){
   title('Platzigram')
   var main = document.getElementById('main-container');
 
@@ -14,40 +13,13 @@ page('/', header, loadPicturesFetch, function(ctx, next){
   empty(main).appendChild(template(ctx.pictures));
 })
 
-function loadPictures(ctx, next){
-  request
-    .get('/api/pictures')
-    .end(function(err, res) {
-      if (err) return console.log(err);
 
-      ctx.pictures = res.body;
-      next();
-    })
-}
 
-function loadPicturesAxios(ctx, next){
-  axios
-    .get('/api/pictures')
-    .then(function(res) {
-      ctx.pictures = res.data;
-      next();
-    })
-    .catch(function(err){
-      console.log(err);
-    })
-}
-
-function loadPicturesFetch(ctx,next){
-  fetch('/api/pictures')
-  .then(function(res){
-    return res.json();
-  })
-  .then(function(pictures){
-    ctx.pictures = pictures;
+async function asyncLoad(ctx, next){
+  try{
+    ctx.pictures = await fetch('/api/pictures').then(res => res.json())
     next();
-  })
-  .catch(function(err){
-    console.log(err);
-  })
-
+  } catch (err){
+    return console.log(err);
+  }
 }
